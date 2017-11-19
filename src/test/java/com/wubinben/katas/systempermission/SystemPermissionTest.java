d63858437f3291a652dac444de7f5af455b5d959
Ben Wu <wubin28@gmail.com>
2017-11-19 22:39:27 +0800

    finish test should_be_the_same_unix_admin_to_grant_after_unix_permission_claimed()

--- src/test/java/com/wubinben/katas/systempermission/SystemPermissionTest.java
+++ src/test/java/com/wubinben/katas/systempermission/SystemPermissionTest.java
@@ -127,7 +127,16 @@ public class SystemPermissionTest {
         assertEquals(SystemPermission.GRANTED, systemPermission.getState());
     }
 
-    // should_be_the_same_unix_admin_to_grant_after_unix_permission_claimed
+    @Test(expected = IllegalArgumentException.class)
+    public void should_be_the_same_unix_admin_to_grant_after_unix_permission_claimed() throws Exception {
+        SystemPermission systemPermission = new SystemPermission(new User());
+        systemPermission.claimedBy(new SystemAdmin());
+        systemPermission.unixRequestedBy(new User());
+        systemPermission.unixClaimedBy(new UnixAdmin());
+
+        systemPermission.unixGrantedBy(new UnixAdmin());
+    }
+
     // should_be_denied_when_unix_denied_by_unix_admin_after_unix_permission_claimed
     // should_be_the_same_unix_admin_to_deny_after_unix_permission_claimed
     // should_be_unix_permission_claimed_when_unix_claimed_by_admin_after_unix_permission_requested
